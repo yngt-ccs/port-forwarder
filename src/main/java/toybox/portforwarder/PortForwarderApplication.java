0ca1a5fb68b33df09ec18de2416cdc999060d645
yngt-ccs <ynagata.ccs@gmail.com>
2020-02-11 16:14:33 +0900

    Spring Boot Web project

---
+++ src/main/java/toybox/portforwarder/PortForwarderApplication.java
@@ -0,0 +1,13 @@
+package toybox.portforwarder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortForwarderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortForwarderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toybox/portforwarder/PortForwarderApplicationTests.java
@@ -0,0 +1,13 @@
+package toybox.portforwarder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortForwarderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
